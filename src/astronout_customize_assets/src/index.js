a14b5fe00a9296c1738afd270126e099a7485b5b
Muazzam Kazmi <muazzam_ali@live.com>
2021-12-17 18:22:43 +0500

    Initial commit.

---
+++ src/astronout_customize_assets/src/index.js
@@ -0,0 +1,19 @@
+import { astronout_customize } from "../../declarations/astronout_customize";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await astronout_customize.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
